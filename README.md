0318ea53ae89b4eb4ad61c14c3ee323e722cd965
Marc Wickenden <marc@4armed.com>
2014-06-04 15:08:22 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+## Nessus2Table
+
+This is one of those scripts you end up knocking up to save you time, then can't remember where you put it so next time have to do it again. So we're sharing this one so we always know where it is!
+
+Sometimes you need to include a table of vulnerabilities in a report (particularly PSN Healthcheck reports) with a big list of systems and the issues that affect them.
+
+Rather than do this by hand, this script takes an exported Nessus CSV file and converts it into a CSV file you can open in Excel, then copy/paste into Word to make a nice table.
+
+### Installation
+
+1. Clone this Github repo
+
+    git clone https://github.com/4armed/nessus2table.git
+
+2. Install the dependencies (there's only one - trollop)
+
+    cd nessus2table
+    bundle install
+
+3. Run the ruby script with the location of your exported Nessus CSV and the filename you'd like the output in.
+
+   ruby nessus2table.rb -i <NessusCVSfile> -o <TargetCVSFile>
+
+
+That's it.
+
+Any questions ping us on Twitter @4ARMED or @marcwickenden.
---
+++ nessus2table.rb
@@ -0,0 +1,85 @@
+#!/usr/bin/env ruby
+
+begin
+  require 'csv'
+  require 'trollop'
+rescue LoadError => e
+  puts "think you've got a gem problem old boy: #{e}"
+  puts "Run bundle install"
+  exit 1
+end
+
+@opts = Trollop::options do
+  opt :input_file, "Nessus CSV input file", :type => :string, :required => true
+  opt :output_file, "Output CSV file", :type => :string, :required => true
+  opt :verbose, "Be verbose"
+end
+
+def log(message)
+  puts "[*] #{message}" if @opts[:verbose]
+end
+
+csv = CSV.parse(File.read(@opts[:input_file]), :headers => true, :header_converters => :symbol)
+
+minimum_cvss_score = 7
+
+hosts_vulns = { 'IP Address' => [] }
+
+csv.each do |row|
+  unless row[:cvss].to_i < minimum_cvss_score
+
+    # Build a list of IP address
+    hosts_vulns['IP Address'] |= [row[:host]]
+
+    # combine vulnerability name with CVE if available
+    vuln_title = "#{row[:name]} (#{row[:cve]})" unless row[:cve].empty?
+    vuln_title = vuln_title || row[:name]
+
+    if hosts_vulns.key?(vuln_title)
+      log("Adding host #{row[:host]} to new vulnerability: #{row[:host]}")
+      hosts_vulns[vuln_title] << row[:host]
+    else
+      log("Adding host #{row[:host]} to vulnerability #{hosts_vulns[vuln_title]}")
+      hosts_vulns[vuln_title] = [row[:host]]
+    end
+  end
+end
+
+# Now we generate our new CSV - this is the header row
+csv_output = [ hosts_vulns.keys ]
+
+# # The vulns go on the top row with a blank first column
+hosts_vulns['IP Address'].each do |ip|
+  host_row = [ ip ]
+  hosts_vulns.keys.each do |vuln|
+    # skip the IP Address column
+    next if vuln == 'IP Address'
+
+    log("Processing vulnerability #{vuln}")
+
+    if hosts_vulns[vuln].include?(ip)
+      host_row << "X"
+    else
+      host_row << ""
+    end
+  end
+
+  # Append our new row to the output array
+  csv_output << host_row
+end
+
+# Now write to a file
+log("Writing output to #{@opts[:output_file]}")
+CSV.open(@opts[:output_file], 'wb') do |csv_file|
+  csv_output.each do |row|
+    csv_file  << row
+  end
+end
+
+log("Completed")
+
+
+
+
+
+
